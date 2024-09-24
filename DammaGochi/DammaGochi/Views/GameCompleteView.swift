738eebd977594ca804deb5dcf39aa0a964c0ee08
sueun kim <doms@kakao.com>
2024-09-24 14:36:08 +0900

    [feat]: animation rotate 추가

---
+++ DammaGochi/DammaGochi/Views/GameCompleteView.swift
@@ -0,0 +1,52 @@
+//
+//  GameCompleteView.swift
+//  DammaGochi
+//
+//  Created by Soom on 9/24/24.
+//
+
+import SwiftUI
+
+struct GameCompleteView: View {
+    @State var isAnimating: Bool = false
+    @State private var rotateCircle = false
+
+    @Binding var showCircle: Bool
+    
+    var body: some View {
+        VStack {
+            if showCircle {
+                Image("coin")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .aspectRatio(contentMode: .fit)
+                    .offset(y: isAnimating ? -UIScreen.main.bounds.height / 2 + 350 : UIScreen.main.bounds.height / 2)
+                    .rotation3DEffect(
+                        .degrees(rotateCircle ? 360 : 0),
+                           axis: (x: 0.0, y: 1.0, z: 0.0),
+                           anchor: .center,
+                           anchorZ: 0,
+                           perspective: 1)
+                    .animation(.easeInOut(duration: 0.8), value: isAnimating)
+                    .animation(.linear(duration: 1).delay(0.5), value: rotateCircle)
+                
+                    .onAppear {
+                        isAnimating = true
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                            rotateCircle = true
+                        }
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                            rotateCircle = false
+                            isAnimating = false
+                            showCircle = false
+                        }
+                    }
+            }
+        }
+    }
+}
+
+
+#Preview {
+    GameCompleteView(showCircle: .constant(false))
+}
--- DammaGochi/DammaGochi/Views/GameView.swift
+++ DammaGochi/DammaGochi/Views/GameView.swift
@@ -11,23 +11,66 @@ import SwiftUI
 struct GameView: View {
     let gridItem = Array(repeating: GridItem(.flexible()), count: 2)
     let gameImageName: [String] = ["game1", "game2", "game3", "game4","game5","game6"]
+    @State var gameCompleted: Bool = false
+    @State var showCircle: Bool = false
     var body: some View {
-        ScrollView{
-            LazyVGrid(columns: gridItem) {
-                ForEach(gameImageName, id: \.self) { gameImageName in
-                    Image(gameImageName)
-                        .resizable()
-                        .frame(height: 300)
-                        .aspectRatio(contentMode: .fit)
+        VStack{
+            ScrollView{
+                LazyVGrid(columns: gridItem) {
+                    ForEach(gameImageName, id: \.self) { gameImageName in
+                        Image(gameImageName)
+                            .resizable()
+                            .frame(height: 300)
+                            .aspectRatio(contentMode: .fit)
+                            .overlay {
+                                VStack{
+                                    Spacer()
+                                    gameStartButton()
+                                }
+                            }
+                    }
                 }
+            }
+            .overlay{
+                GameCompleteView(showCircle: $gameCompleted)
             }
         }
         .scrollIndicators(.hidden)
+        .navigationTitle("Game")
+        .navigationBarTitleDisplayMode(.inline)
         .padding(.horizontal,10)
+        .toolbar{
+            ToolbarItem(placement: .topBarTrailing){
+                HStack{
+                    Image("coin")
+                        .resizable()
+                        .frame(width: 25, height: 25)
+                    Text("500")
+                }
+            }
+        }
+    }
+    @ViewBuilder
+    func gameStartButton() -> some View {
+        VStack{
+            Button{
+                gameCompleted.toggle()
+            }label: {
+                Text("Start")
+                    .padding(.vertical, 10)
+                    .padding(.horizontal,55)
+                    .foregroundStyle(.white)
+                    .background(.green.gradient)
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                    .padding(.bottom, 10)
+            }
+        }
     }
 }
 
 
 #Preview {
-    GameView()
+    NavigationStack{
+        GameView()
+    }
 }
