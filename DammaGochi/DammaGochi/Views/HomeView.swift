217d28f6b9f5f868dcf364a411ad1f24601d71e1
최승호 <sts06017@naver.com>
2024-09-24 11:35:50 +0900

    HomeTab - ARKit 적용

--- DammaGochi/DammaGochi/Views/HomeView.swift
+++ DammaGochi/DammaGochi/Views/HomeView.swift
@@ -6,9 +6,55 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct HomeView: View {
+    @State private var isShowAR: Bool = false
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            if isShowAR {
+                ARViewContainer(isShowAR: $isShowAR)
+                    .onAppear {
+                        requestCameraPermission()
+                    }
+                    .onDisappear() {
+                        isShowAR = false
+                    }
+            }
+            
+            VStack{
+                Spacer()
+                HStack(spacing: 20){
+                    Button(action: {
+                        isShowAR.toggle()
+                    }) {
+                        ZStack {
+                            Circle()
+                                .fill(Color.cyan)
+                                .frame(width: 50, height: 50)
+                            Image(systemName: "web.camera")
+                        }
+                    }
+                    .padding(.leading, 20)
+                    .padding(.bottom, 20)
+                    Spacer()
+                }
+            }
+        }
     }
+    
+    func requestCameraPermission() {
+        AVCaptureDevice.requestAccess(for: .video) { granted in
+            if granted {
+                print("Access granted")
+            } else {
+                print("Access denied")
+            }
+        }
+    }
+}
+
+#Preview {
+    HomeView()
 }
