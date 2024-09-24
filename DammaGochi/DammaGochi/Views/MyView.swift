88361ad39cae4598b0d762cc558db0afb4186382
Leesooooyoung <soyoung3466@gmail.com>
2024-09-24 16:34:13 +0900

    [chore] Lab 부분 수정
    (커뮤니티 정보 -> 커뮤니티 정보, 앱 연동, 굿즈, 다이어리)

--- DammaGochi/DammaGochi/Views/MyView.swift
+++ DammaGochi/DammaGochi/Views/MyView.swift
@@ -6,12 +6,11 @@
 //
 
 import SwiftUI
-import SwiftSoup
 
 struct MyView: View {
     var examplePets: [PetExam] = [PetExam(name: "댕", age: 5, gender: "♀", personality: "매우 귀엽게 생긴 외모에 그렇지 못한 성격입니다...!", imageStr: "https://emojigraph.org/media/apple/dog-face_1f436.png"), PetExam(name: "고양", age: 2, gender: "♂", personality: "고양이인척 하는 토끼입니다...!", imageStr: "https://emojigraph.org/media/apple/rabbit-face_1f430.png")]
     
-    var exampleSites: Dictionary = ["https://images.mypetlife.co.kr/content/icons/bemypet-logo.png" : "https://mypetlife.co.kr/", "https://images.mypetlife.co.kr/content/uploads/2022/09/16111809/%EC%95%A0%ED%94%84%ED%84%B08%EA%B0%9C-%EB%A8%B9%EC%97%88%EC%9D%84%EB%95%8C-edited-390x220.png" : "https://mypetlife.co.kr/134768/", "https://images.mypetlife.co.kr/content/uploads/2024/03/11191702/AdobeStock_233935535-390x220.jpeg" : "https://mypetlife.co.kr/151174/#google_vignette", "https://images.mypetlife.co.kr/content/uploads/2022/05/24142922/2103080693_4bbde53c24_c-390x220.jpg" : "https://mypetlife.co.kr/117887/"]
+    var exampleLabContents: [LabContent] = [LabContent(title: "반려동물 커뮤니티", imageStr: "https://images.mypetlife.co.kr/content/icons/bemypet-logo.png", address: "https://mypetlife.co.kr/"), LabContent(title: "앱 굿즈 샵", imageStr: "https://img.bemypet.kr/content/2023/12/13150538/bemypet-friends-981x1024.png", address: "https://www.idus.com/v2/product/08c7aec9-c3ef-452f-bc15-98a0dbf81d38?search_word=%EB%B0%98%EB%A0%A4%EB%8F%99%EB%AC%BC%EA%B5%BF%EC%A6%88&keyword_channel="), LabContent(title: "연동 앱", imageStr: "https://webimage.10x10.co.kr/image/basic600/387/B003875310.jpg", address: "https://apps.apple.com/kr/app/%ED%8E%AB%ED%94%BC-%ED%8F%AC%EC%9D%B8%ED%8A%B8-%EC%A0%81%EB%A6%BD%ED%98%95-%EA%B0%95%EC%95%84%EC%A7%80-%EC%82%B0%EC%B1%85%EC%95%B1/id1487605660"), LabContent(title: "펫 다이어리", imageStr: "https://thumbnail.10x10.co.kr/webimage/image/basic600/387/B003875308.jpg?cmd=thumb&w=400&h=400&fit=true&ws=false", address: "https://petcarediary.org/?page=login")]
     
     var body: some View {
         NavigationStack {
@@ -22,7 +21,7 @@ struct MyView: View {
                 }
                 
                 Section("Lab") {
-                    LabView(sites: exampleSites)
+                    LabView(contents: exampleLabContents)
                         .listRowSeparator(.hidden)
                 }
             }
@@ -41,7 +40,7 @@ struct MyView: View {
 }
 
 struct LabView: View {
-    var sites: Dictionary<String, String>
+    var contents: [LabContent]
     var columns: [GridItem] = [
         GridItem(.flexible()),
         GridItem(.flexible())
@@ -52,25 +51,17 @@ struct LabView: View {
     var body: some View {
         ScrollView {
             LazyVGrid(columns: columns) {
-                ForEach(sites.sorted(by: >), id: \.key) {
-                    imageLink, webLink in
-                    Link(destination: URL(string: webLink)!) {
-                        AsyncImage(url: URL(string: imageLink)) { image in
+                ForEach(contents) { content in
+                    Link(destination: URL(string: content.address)!) {
+                        AsyncImage(url: URL(string: content.imageStr)) { image in
                             image
                                 .resizable()
+                                .frame(width: 100, height: 100)
                                 .aspectRatio(contentMode: .fit)
                                 .foregroundStyle(.white)
                                 .clipShape(.rect(cornerRadius: 10))
                             
-                            // 콘텐츠가 없어서 반려동물 플랫폼 사이트 찾아서 따왔습니다
-                            // HTML에서 타이틀 가져오는 방식 (SwiftSoup 라이브러리 사용)
-                            let url = URL(string: webLink)!
-                            let html = try? String(contentsOf: url, encoding: .utf8)
-                            let doc = try? SwiftSoup.parse(html!)
-                            let title = try? doc?.select("title")
-                            let str = try? title?.text()
-                            
-                            Text(str ?? "")
+                            Text(content.title)
                             
                             Spacer()
                         } placeholder: {
@@ -150,6 +141,13 @@ struct PetExam: Identifiable {
     // 일단은 어싱크 이미지로 주소 넣어놨습니다
     // TODO: 로티에서 이미지 따오기
     var imageStr: String
+}
+
+struct LabContent: Identifiable {
+    var id: UUID = UUID()
+    var title: String
+    var imageStr: String
+    var address: String
 }
 
 #Preview {
