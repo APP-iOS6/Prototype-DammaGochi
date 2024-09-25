2624fcd2acafeb4548c2ae0cf0bdcf20b23f22e3
sueun kim <doms@kakao.com>
2024-09-25 15:51:36 +0900

    [chore]: 게임뷰 상세정보 추가

--- DammaGochi/DammaGochi/Views/GameView.swift
+++ DammaGochi/DammaGochi/Views/GameView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct GameView: View {
     let gridItem = Array(repeating: GridItem(.flexible()), count: 2)
     let gameImageName: [String] = ["game1", "game2", "game3", "game4","game5","game6"]
+    let gameName: [String] = ["산책하기", "올림픽 하기", "옷 입히기", "친구 만들기", "간식 주기", "공 안떨어뜨리기"]
     @State var gameCompleted: Bool = false
     @State var showCircle: Bool = false
     @State var isGameEnd: Bool = false
@@ -20,13 +21,28 @@ struct GameView: View {
             VStack{
                 ScrollView{
                     LazyVGrid(columns: gridItem) {
-                        ForEach(gameImageName, id: \.self) { gameImageString in
+                        ForEach(gameImageName.indices, id: \.self) { index in
                             NavigationLink{
                                 GamePlayView(gameComplted: $gameCompleted)
                             }label:{
-                                gameImageName(gameImageName: gameImageString)
+                                gameImageName(gameImageName: gameImageName[index])
                                     .overlay{
                                         VStack{
+                                            HStack{
+                                                Spacer()
+                                                Image("coin")
+                                                    .resizable()
+                                                    .frame(width: 15, height: 15)
+                                                Text("10")
+                                                    .foregroundStyle(.black)
+                                                    .font(.body)
+                                                    .fontWeight(.bold)
+                                            }
+                                            .padding(5)
+                                            Text(gameName[index])
+                                                .fontWeight(.heavy)
+                                                .font(.system(size: 28))
+                                                .foregroundStyle(.white)
                                             Spacer()
                                             Text("Start")
                                                 .padding(.vertical, 10)
@@ -39,26 +55,23 @@ struct GameView: View {
                                     }
                             }
                         }
-                    }
-                    .overlay{
-                        GameCompleteView(showCircle: $gameCompleted)
+                        .overlay{
+                            GameCompleteView(showCircle: $gameCompleted)
+                        }
                     }
                 }
-                .onAppear{
-                    gameCompleted = true
-                }
-            }
-            .scrollIndicators(.hidden)
-            .navigationTitle("Game")
-            .navigationBarTitleDisplayMode(.inline)
-            .padding(.horizontal,10)
-            .toolbar{
-                ToolbarItem(placement: .topBarTrailing){
-                    HStack{
-                        Image("coin")
-                            .resizable()
-                            .frame(width: 25, height: 25)
-                        Text("\(coinManager.coin)")
+                .scrollIndicators(.hidden)
+                .navigationTitle("Game")
+                .navigationBarTitleDisplayMode(.inline)
+                .padding(.horizontal,10)
+                .toolbar{
+                    ToolbarItem(placement: .topBarTrailing){
+                        HStack{
+                            Image("coin")
+                                .resizable()
+                                .frame(width: 25, height: 25)
+                            Text("\(coinManager.coin)")
+                        }
                     }
                 }
             }
@@ -70,6 +83,7 @@ struct GameView: View {
             .resizable()
             .frame(height: 300)
             .aspectRatio(contentMode: .fit)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
     }
 }
 
