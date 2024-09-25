5315b6199510e1e58f4fecacb4f1f37dbb764f98
saul1113 <clclshtus@gmail.com>
2024-09-25 16:24:36 +0900

    store

--- DammaGochi/DammaGochi/Views/StoreView.swift
+++ DammaGochi/DammaGochi/Views/StoreView.swift
@@ -23,17 +23,18 @@ struct StoreView: View {
     //    let products = Array(repeating: "상품", count: 10)
     let products = [
         Product(name: "1등급 댕댕이 펫밀크", price: 10, imageName: "petMilk", category: "간식"),
-        Product(name: "1등급 냥냥이 츄르", price: 10, imageName: "Chur", category: "간식"),
+        Product(name: "1등급 냥냥이 츄르", price: 10, imageName: "chur", category: "간식"),
         Product(name: "냥냥이 낚시", price: 50, imageName: "catFish", category: "장난감"),
         Product(name: "댕댕이 공놀이", price: 50, imageName: "dogBall", category: "장난감"),
         Product(name: "레드 스트라이프 티셔츠", price: 150, imageName: "red", category: "의류"),
         Product(name: "블루 스트라이프 티셔츠", price: 150, imageName: "blue", category: "의류"),
     ]
     
-    @State private var userCoins: Int = 100
+    @StateObject private var coinManager = CoinManager()
     @State private var showAlert: Bool = false
     @State private var selectedProduct: Product?
     @State private var ownedProducts: [UUID] = []   //구매된 상품 ID로 저장하기 위함
+    @State private var addItem: [UUID: Int] = [:]
     
     private var filteredProducts: [Product] {
         if selected == "전체" {
@@ -79,14 +80,37 @@ struct StoreView: View {
                                     
                                     
                                     HStack {
-                                        Text("\(product.price)")
+                                        Text("\(product.price) x \(addItem[product.id] ?? 1) = \(product.price * (addItem[product.id] ?? 1))")
                                             .foregroundColor(.black)
                                             .font(.headline)
                                         Image("coin")
                                             .resizable()
                                             .frame(width: 20, height: 20)
                                     }
-                                    
+                                    HStack {
+                                        Button(action: {
+                                            let currentQuantity = addItem[product.id] ?? 1
+                                            if currentQuantity > 1 {
+                                                addItem[product.id] = currentQuantity - 1
+                                            }
+                                        }) {
+                                            Image(systemName: "minus.circle")
+                                                .font(.title)
+                                                .foregroundColor(.red)
+                                        }
+                                        
+                                        Text("\(addItem[product.id] ?? 1)")
+                                            .padding(.horizontal, 10)
+                                        
+                                        Button(action: {
+                                            let currentQuantity = addItem[product.id] ?? 1
+                                            addItem[product.id] = currentQuantity + 1
+                                        }) {
+                                            Image(systemName: "plus.circle")
+                                                .font(.title)
+                                                .foregroundColor(.green)
+                                        }
+                                    }
                                     Button(action: {
                                         selectedProduct = product
                                         showAlert = true
@@ -94,12 +118,12 @@ struct StoreView: View {
                                         Text("구매하기")
                                             .font(.caption)
                                             .padding(5)
-                                            .background(ownedProducts.contains(product.id) ? Color.gray : Color.purple)
+                                            .background(Color.purple)
                                             .foregroundColor(.white)
                                             .cornerRadius(5)
                                     }
                                     .padding(.top, 5)
-                                    .disabled(ownedProducts.contains(product.id))   //구매완료된 상품은 비활성화
+                                    //                                    .disabled(ownedProducts.contains(product.id))   //구매완료된 상품은 비활성화
                                 }
                                 .padding()
                                 .background(Color(UIColor.systemGray6))
@@ -120,7 +144,7 @@ struct StoreView: View {
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     HStack {
-                        Text("\(userCoins)")
+                        Text("\(coinManager.coin)")
                             .font(.headline)
                         Image("coin")
                             .resizable()
@@ -131,15 +155,17 @@ struct StoreView: View {
             }
             .alert(isPresented: $showAlert) {
                 if let product = selectedProduct {
-                    if userCoins >= product.price {
-                        return Alert(
-                            title: Text("구매 확인"),
-                            message: Text("\(product.price) 코인을 사용하여 구매하시겠습니까?"),
-                            primaryButton: .default(Text("확인"), action: {
-                                purchaseProduct(product: product)
-                            }),
-                            secondaryButton: .cancel(Text("취소"))
-                        )
+                                    let quantity = addItem[product.id] ?? 1
+                                    let totalPrice = product.price * quantity
+                                    if coinManager.coin >= totalPrice {
+                                        return Alert(
+                                            title: Text("구매 확인"),
+                                            message: Text("\(totalPrice) 코인을 사용하여 \(quantity)개를 구매하시겠습니까?"),
+                                            primaryButton: .default(Text("확인"), action: {
+                                                purchaseProduct(product: product, quantity: quantity)
+                                            }),
+                                            secondaryButton: .cancel(Text("취소"))
+                                        )
                     } else {
                         return Alert(
                             title: Text("코인이 부족합니다"),
@@ -158,11 +184,13 @@ struct StoreView: View {
             }
         }
     }
-    private func purchaseProduct(product: Product) {
-        userCoins -= product.price
-        ownedProducts.append(product.id)
-        print("구매성공! 남은코인: \(userCoins)")
-    }
+    private func purchaseProduct(product: Product, quantity: Int) {
+            let totalPrice = product.price * quantity // 총 가격 계산
+            coinManager.storeCoin(price: totalPrice) // 총 가격만큼 코인 차감
+            ownedProducts.append(product.id)
+            addItem[product.id] = 1
+            print("구매성공! 남은코인: \(coinManager.coin)")
+        }
 }
 
 struct StoreView_Previews: PreviewProvider {
